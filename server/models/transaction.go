c4a782de4491ccab2bbfe09feee2046d2be8978f
Jody-septiawan <jody.septiawan5@gmail.com>
2022-08-13 09:51:37 +0700

    Prepare

--- server/models/transaction.go
+++ server/models/transaction.go
@@ -5,7 +5,7 @@ import "time"
 type Transaction struct {
 	ID        int                  `json:"id" gorm:"primary_key:auto_increment"`
 	ProductID int                  `json:"product_id"`
-	Product   ProductResponse      `json:"product"`
+	Product   ProductResponse      `json:"product" gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	BuyerID   int                  `json:"buyer_id"`
 	Buyer     UsersProfileResponse `json:"buyer"`
 	SellerID  int                  `json:"seller_id"`
